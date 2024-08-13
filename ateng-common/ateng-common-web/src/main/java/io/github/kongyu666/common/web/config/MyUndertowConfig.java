b63da7ef15c9280db76b6ff3b37fd3d0e262565c
孔余 <2385569970@qq.com>
2024-08-13 20:28:23 +0800

    first commit

--- ateng-common/ateng-common-web/src/main/java/io/github/kongyu666/common/web/config/MyUndertowConfig.java
+++ ateng-common/ateng-common-web/src/main/java/io/github/kongyu666/common/web/config/MyUndertowConfig.java
@@ -1,12 +1,14 @@
 package io.github.kongyu666.common.web.config;
 
 import io.github.kongyu666.common.core.factory.YmlPropertySourceFactory;
+import io.github.kongyu666.common.core.utils.SpringUtils;
 import io.undertow.server.DefaultByteBufferPool;
 import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.task.VirtualThreadTaskExecutor;
 
 /**
  * undertow 配置
@@ -17,16 +19,20 @@ import org.springframework.context.annotation.PropertySource;
 @AutoConfiguration
 @PropertySource(value = "classpath:common-web.yml", factory = YmlPropertySourceFactory.class)
 public class MyUndertowConfig implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
-    /**
-     * 设置 Undertow 的 websocket 缓冲池
-     */
     @Override
     public void customize(UndertowServletWebServerFactory factory) {
         // 默认不直接分配内存 如果项目中使用了 websocket 建议直接分配
         factory.addDeploymentInfoCustomizers(deploymentInfo -> {
             WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
-            webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(false, 512));
+            webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(true, 1024));
             deploymentInfo.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo);
+            // 使用虚拟线程
+            if (SpringUtils.isVirtual()) {
+                VirtualThreadTaskExecutor executor = new VirtualThreadTaskExecutor("undertow-");
+                deploymentInfo.setExecutor(executor);
+                deploymentInfo.setAsyncExecutor(executor);
+            }
         });
     }
+
 }
