81d4fa82446695b654be0a31916b7e4e8dfe1897
孔余 <2385569970@qq.com>
2024-10-11 13:43:07 +0800

    修改：校验框架 快速失败模式

--- ateng-common/ateng-common-core/src/main/java/io/github/kongyu666/common/core/config/ValidatorConfig.java
+++ ateng-common/ateng-common-core/src/main/java/io/github/kongyu666/common/core/config/ValidatorConfig.java
@@ -1,13 +1,11 @@
 package io.github.kongyu666.common.core.config;
 
+import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.hibernate.validator.HibernateValidator;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import java.util.Properties;
 
 /**
  * 校验框架配置类
@@ -21,20 +19,12 @@ public class ValidatorConfig {
      * 配置校验框架 快速返回模式
      */
     @Bean
-    public Validator validator(MessageSource messageSource) {
-        try (LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean()) {
-            // 国际化
-            factoryBean.setValidationMessageSource(messageSource);
-            // 设置使用 HibernateValidator 校验器
-            factoryBean.setProviderClass(HibernateValidator.class);
-            Properties properties = new Properties();
-            // 设置 快速异常返回
-            properties.setProperty("hibernate.validator.fail_fast", "true");
-            factoryBean.setValidationProperties(properties);
-            // 加载配置
-            factoryBean.afterPropertiesSet();
-            return factoryBean.getValidator();
-        }
+    public Validator validator() {
+        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                .failFast(true)  // 启用快速失败模式
+                .buildValidatorFactory();
+        return validatorFactory.getValidator();
     }
 
 }
